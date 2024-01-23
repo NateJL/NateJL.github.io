70e83a14b5b0fd418b4abe0f5bd2e0651e9d1306
NateJL <nathan.john.larson@gmail.com>
2024-01-23 13:30:34 -0700

    fixed typo

--- scripts/blog_script.js
+++ scripts/blog_script.js
@@ -4,8 +4,8 @@ console.log(data);
 const blogData = JSON.parse(data);
 
 let text = "<table border='1'>";
-for (let x in myObj) {
-	text += "<tr><td>" + myObj[x].name + "</td></tr>";
+for (let x in blogData) {
+	text += "<tr><td>" + blogData[x].name + "</td></tr>";
 }
 text += "</table>"    
 document.getElementById("blog_content").innerHTML = text;
