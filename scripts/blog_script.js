f3bd2cffb31d60089f75fd2a5ed53f806bbe9d73
NateJL <nathan.john.larson@gmail.com>
2024-01-23 13:27:00 -0700

    added json parse to table

--- scripts/blog_script.js
+++ scripts/blog_script.js
@@ -1,2 +1,11 @@
 import data from '../data/blog_data.json' assert { type: 'json' };
 console.log(data);
+
+const blogData = JSON.parse(data);
+
+let text = "<table border='1'>";
+for (let x in myObj) {
+	text += "<tr><td>" + myObj[x].name + "</td></tr>";
+}
+text += "</table>"    
+document.getElementById("blog_content").innerHTML = text;
