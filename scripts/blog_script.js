9fdde827bf0506c482e866b6cde050e06a23a668
NateJL <nathan.john.larson@gmail.com>
2024-01-23 13:39:26 -0700

    fixed json reference

--- scripts/blog_script.js
+++ scripts/blog_script.js
@@ -1,11 +1,11 @@
 import data from '../data/blog_data.json' assert { type: 'json' };
 console.log(data);
 
-const blogData = JSON.parse(data);
+const blogData = JSON.parse(data.posts);
 
 let text = "<table border='1'>";
-for (let x in data) {
-	text += "<tr><td>" + data[x].title + "</td></tr>";
+for (let x in blogData) {
+	text += "<tr><td>" + blogData[x].title + "</td></tr>";
 }
 text += "</table>"    
 document.getElementById("blog_content").innerHTML = text;
