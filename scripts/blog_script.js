6e9df91fc6da812075e96b956b03ce052b587bac
NateJL <nathan.john.larson@gmail.com>
2024-01-23 13:35:13 -0700

    parse fix

--- scripts/blog_script.js
+++ scripts/blog_script.js
@@ -4,8 +4,8 @@ console.log(data);
 const blogData = JSON.parse(data);
 
 let text = "<table border='1'>";
-for (let x in blogData) {
-	text += "<tr><td>" + blogData[x].name + "</td></tr>";
+for (let x in data) {
+	text += "<tr><td>" + data[x].title + "</td></tr>";
 }
 text += "</table>"    
 document.getElementById("blog_content").innerHTML = text;
