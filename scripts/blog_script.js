61bf81c68325bb4375cc6a5bcc0901b402def3f9
NateJL <nathan.john.larson@gmail.com>
2024-01-23 13:44:48 -0700

    json fix

--- scripts/blog_script.js
+++ scripts/blog_script.js
@@ -1,7 +1,7 @@
 import data from '../data/blog_data.json' assert { type: 'json' };
 console.log(data);
 
-const blogData = JSON.parse(data.posts);
+const blogData = JSON.parse(data[0].posts);
 
 let text = "<table border='1'>";
 for (let x in blogData) {
