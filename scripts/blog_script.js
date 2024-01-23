59f51382691700aface51c29289e970dab83bd07
NateJL <nathan.john.larson@gmail.com>
2024-01-23 14:01:07 -0700

    update json parse

--- scripts/blog_script.js
+++ scripts/blog_script.js
@@ -3,10 +3,23 @@ console.log(data);
 
 //const blogData = JSON.parse(data[0].posts);
 
+let text = "<div>";
 for(let i = 0; i < data.length; i++) {
     let obj = data[i];
+    text += "<h2>";
+    text += obj.title;
+    text += "</h2>";
+    text += "<h4>";
+    text += obj.date;
+    text += "</h4>";
+    text += "<p>";
+    text += obj.content;
+    text += "</p>";
 
-    console.log(obj.id);
+    console.log(obj.title);
+    console.log(obj.date);
+    console.log(obj.content);
 }
+text += "</div>";
     
-//document.getElementById("blog_content").innerHTML = text;
+document.getElementById("blog_content").innerHTML = text;
